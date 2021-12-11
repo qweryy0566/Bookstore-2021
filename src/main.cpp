1ffb7813a60761116fe6dcf386c3e0551276876a
qweryy0566 <qweryy0566@qq.com>
2021-12-12 01:19:45 +0800

    滚回 main

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,25 @@ using std::cout;
 
 int main() {
   std::ios::sync_with_stdio(0);
-  
+  BlockList list("test");
+  int Q, val;
+  string opt, key;
+  for (cin >> Q; Q--;) {
+    cin >> opt >> key;
+    if (opt == "insert") {
+      cin >> val;
+      list.Add(Node(key, val));
+    } else if (opt == "delete") {
+      cin >> val;
+      list.Del(Node(key, val));
+    } else if (opt == "find") {
+      vector<int> ans;
+      if (!list.Query(key, ans))
+        cout << "null";
+      else
+        for (auto it : ans) cout << it << ' ';
+      cout.put('\n');
+    }
+  }
   return 0;
 }
